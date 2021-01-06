c19fd01fbaa99f28303abb811bca5cf72a120713
VladimirZhdanov <ConstHomespb@gmail.com>
2021-01-06 14:19:49 +0300

    fix ui

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,20 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageViewFirst: UIImageView!
     @IBOutlet weak var diceImageViewSecond: UIImageView!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         let array = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
-        diceImageViewFirst.image = array[Int.random(in: 0..<array.capacity)]
+        diceImageViewFirst.image = array.randomElement()
         diceImageViewSecond.image = array[Int.random(in: 0..<array.capacity)]
     }
-
 }
 
